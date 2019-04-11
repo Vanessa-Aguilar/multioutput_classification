48cb9f2e8b1a2ff55fe1bb4e1f163c155f9be988
SM Abdullah <sma.csedu@gmail.com>
2019-04-11 11:09:40 +1000

    Added code comments and the generated CSV file

--- multioutput_classification.py
+++ multioutput_classification.py
@@ -2,7 +2,8 @@
 """
 Created on Mon Apr  8 15:45:47 2019
 
-@author: SM
+@author: SM Abdullah
+@email: sma.csedu@gmail.com
 """
 
 import numpy as np
@@ -12,10 +13,11 @@ import pandas as pd
 X = np.random.random((5000, 294))
 Y = np.random.randint(2, size=(5000, 6))
 
+# Split it for training and testing
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
-# create a random forest model using the default parameters, but n_estimators=300
+# create a random forest model using n_estimators=300, max_features = 'sqrt' and random_state=0 for reproduction
 
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators=300, max_features='sqrt', random_state=0)
@@ -28,12 +30,14 @@ multi_target_classifier = MultiOutputClassifier(classifier, n_jobs=-1)
 
 Y_pred = multi_target_classifier.fit(X_train, Y_train).predict(X_test)
 
-Y_pred_df = pd.DataFrame(Y_pred)
-
 # calculate multioutput precision and loss
 from sklearn.metrics import label_ranking_average_precision_score, label_ranking_loss
 
 avg_precision = label_ranking_average_precision_score(Y_test, Y_pred)
 loss = label_ranking_loss(Y_test, Y_pred)
 
+print('Average precision {0} and loss {1}'.format(avg_precision, loss))
+
+# Convert the prediction into a Pandas dataframe and save it as a CSV file
+Y_pred_df = pd.DataFrame(Y_pred)
 Y_pred_df.to_csv('Prediction.csv')
